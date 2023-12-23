d1268476dca76c239f22d4e34d1a99090273f8ca
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2023-12-23 14:41:55 -0300

    lazy loading, jsonignore, json format

--- src/main/java/com/example/springstudy/config/TestConfig.java
+++ src/main/java/com/example/springstudy/config/TestConfig.java
@@ -1,11 +1,16 @@
 package com.example.springstudy.config;
 
+import java.time.Instant;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.springstudy.entities.Order;
 import com.example.springstudy.entities.User;
+import com.example.springstudy.repositories.OrderRepository;
 import com.example.springstudy.repositories.UserRespository;
 
 @Configuration
@@ -14,6 +19,9 @@ public class TestConfig implements CommandLineRunner{
     
     @Autowired
     private UserRespository userRespository; // extends JpaRepository
+    
+    @Autowired
+    private OrderRepository orderRepository; // extends JpaRepository
 
     @Override
     public void run(String... args) throws Exception {
@@ -21,8 +29,14 @@ public class TestConfig implements CommandLineRunner{
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "11 2 3333 4444", "password");
         User u2 = new User(null, "Bobby Junior", "bobbyjr@gmail.com", "11 2 3333 4444", "password");
 
-        userRespository.save(u1);
-        userRespository.save(u2);
+        userRespository.saveAll(Arrays.asList(u1, u2));
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
     }
 
 }
