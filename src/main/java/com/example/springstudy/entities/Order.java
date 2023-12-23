e9b9edefd2f67d3054cff9fb9ad637267525b3c5
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2023-12-23 14:59:56 -0300

    order status enum

--- src/main/java/com/example/springstudy/entities/Order.java
+++ src/main/java/com/example/springstudy/entities/Order.java
@@ -3,6 +3,7 @@ package com.example.springstudy.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.example.springstudy.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,12 +28,15 @@ public class Order implements Serializable{
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,16 @@ public class Order implements Serializable{
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
@@ -84,6 +98,5 @@ public class Order implements Serializable{
             return false;
         return true;
     }
-
     
 }
