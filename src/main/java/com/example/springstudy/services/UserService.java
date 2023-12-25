f9a4cbe2a08af8e491d6628c56dbf50943be824b
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2023-12-25 17:46:04 -0300

    user delete

--- src/main/java/com/example/springstudy/services/UserService.java
+++ src/main/java/com/example/springstudy/services/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
     public User insert(User user){
         return userRespository.save(user);
     }
+
+    public void delete(Long id){
+        userRespository.deleteById(id);
+    }
 }
